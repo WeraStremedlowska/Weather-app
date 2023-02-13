1db39309e65ea7e76110091e715ec852cf35e8d7
Wera Stremedlowska <Stremedlowska@gmail.com>
2023-02-13 10:21:51 +0100

    update City UI

---
+++ Weather-app/Controller/AddCityViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AddCityViewController.swift
+//  Weather-app
+//
+//  Created by Wera Stremedlowska on 13.02.23.
+//
+
+import UIKit
+
+class AddCityViewContreller: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+    }
+    
+    private func setupViews() {
+        view.backgroundColor = UIColor(white: 0.3, alpha: 0.4)
+    }
+}
